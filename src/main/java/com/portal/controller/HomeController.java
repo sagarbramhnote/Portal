04b31173e19865876fe65307bb201bab9a521a2d
admin1 <sagarbramhnote@gmail.com>
2022-08-01 18:10:20 +0530

    edit our project

--- src/main/java/com/portal/controller/HomeController.java
+++ src/main/java/com/portal/controller/HomeController.java
@@ -1,25 +1,11 @@
 package com.portal.controller;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.portal.entity.Role;
-import com.portal.entity.User;
-import com.portal.entity.UserRole;
-import com.portal.services.UserService;
 
 @Controller
 public class HomeController {
 
-
-	
 	@GetMapping("/")
 	public String index() {
 		
@@ -38,12 +24,10 @@ public class HomeController {
 		return "login";
 	}
 	
-	@GetMapping("/admin/home")
-	public String adminindex() {
-		
+	@GetMapping("/adminhome")
+	public String get()
+	{
 		return "admin/index";
 	}
-	
 
-	
 }
